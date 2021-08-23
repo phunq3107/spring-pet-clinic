7745109e7c0e80ab3faf6283b36545dc86659b3d
phunq3107 <phunguyen3107@gmail.com>
2021-08-23 10:13:08 +0700

    Add vets list and update controller. Closes #11

--- pet-clinic-web/src/main/java/com/phunguyen3107/springpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/phunguyen3107/springpetclinic/controller/VetController.java
@@ -1,14 +1,23 @@
 package com.phunguyen3107.springpetclinic.controller;
 
+import com.phunguyen3107.springpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path = "/vet")
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping(path = {"","/","/index","/index.html"})
-    public String listVets(){
+    public String listVets(Model model ){
+        model.addAttribute("listVets", vetService.findAll());
         return "vet/index";
     }
 }
