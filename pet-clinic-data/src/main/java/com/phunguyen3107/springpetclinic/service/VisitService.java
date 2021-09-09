9bdd43e0b7451d0763373b705341b00b6bad0720
phunq3107 <phunguyen3107@gmail.com>
2021-09-09 16:04:13 +0700

    add spring data jpa service for visit. closes #41

---
+++ pet-clinic-data/src/main/java/com/phunguyen3107/springpetclinic/service/VisitService.java
@@ -0,0 +1,6 @@
+package com.phunguyen3107.springpetclinic.service;
+
+import com.phunguyen3107.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/phunguyen3107/springpetclinic/service/springdatajpa/VisitJpaService.java
@@ -0,0 +1,45 @@
+package com.phunguyen3107.springpetclinic.service.springdatajpa;
+
+import com.phunguyen3107.springpetclinic.model.Visit;
+import com.phunguyen3107.springpetclinic.repository.VisitRepository;
+import com.phunguyen3107.springpetclinic.service.VisitService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class VisitJpaService implements VisitService {
+    private final VisitRepository visitRepository;
+
+    public VisitJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+
+        visitRepository.findAll().forEach(visits::add);
+
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
